1165ee30524a799667ad6aaa4ce8515e00a5e65d
Geoff Levand <geoff@infradead.org>
2021-05-05 18:19:17 -0700

    Fix make-age-list
    Signed-off-by: Geoff Levand <geoff@infradead.org>

--- scripts/make-age-list.sh
+++ scripts/make-age-list.sh
@@ -7,16 +7,16 @@ usage() {
 	echo "${script_name} (audx) - Create an age list of collection." >&2
 	echo "Usage: ${script_name} [flags] top-dir" >&2
 	echo "Option flags:" >&2
-	echo "  -o --output-file - Playlist output file. Default: '${out_file}'." >&2
-	echo "  -h --help        - Show this help and exit." >&2
-	echo "  -v --verbose     - Verbose execution." >&2
-	echo "  -g --debug       - Extra verbose execution." >&2
+	echo "  -o --out-file - Playlist output file. Default: '${out_file}'." >&2
+	echo "  -h --help     - Show this help and exit." >&2
+	echo "  -v --verbose  - Verbose execution." >&2
+	echo "  -g --debug    - Extra verbose execution." >&2
 	eval "${old_xtrace}"
 }
 
 process_opts() {
 	local short_opts="o:hvg"
-	local long_opts="output-file:,help,verbose,debug"
+	local long_opts="out-file:,help,verbose,debug"
 
 	local opts
 	opts=$(getopt --options ${short_opts} --long ${long_opts} -n "${script_name}" -- "$@")
@@ -26,7 +26,7 @@ process_opts() {
 	while true ; do
 		#echo "${FUNCNAME[0]}: @${1}@ @${2}@"
 		case "${1}" in
-		-o | --output-file)
+		-o | --out-file)
 			out_file="${2}"
 			shift 2
 			;;
@@ -94,7 +94,7 @@ fi
 check_top_dir "${top_dir}"
 top_dir="$(realpath -e "${top_dir}")"
 
-find . -type f -name '*.flac' -exec ls -l --time-style='+%Y.%m.%d' {} \; \
+(cd "${top_dir}" && find . -type f -name '*.flac' -exec ls -l --time-style='+%Y.%m.%d' {} \;) \
 	| grep -E --only-matching '[[:digit:]]{4}(\.[[:digit:]]{2}){2} .*$' \
 	| sort > "${out_file}"
 
